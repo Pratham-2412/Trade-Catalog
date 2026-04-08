4ef978bacea25b46ec9adc00fbc79d90f798e593
Pratham-2412 <pratham17306@gmail.com>
2026-04-08 13:19:37 +0530

    fix backend

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,43 +1,11 @@
-const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
-
-const oAuth2Client = new google.auth.OAuth2(
-  process.env.EMAIL_CLIENT_ID,
-  process.env.EMAIL_CLIENT_SECRET,
-  "https://developers.google.com/oauthplayground"
-);
-
-oAuth2Client.setCredentials({
-  refresh_token: process.env.EMAIL_REFRESH_TOKEN,
-});
-
-const sendMail = async ({ to, subject, html }) => {
-  try {
-    const accessToken = await oAuth2Client.getAccessToken();
-    const token = accessToken?.token || accessToken;
-
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: process.env.EMAIL_USER,
-        clientId: process.env.EMAIL_CLIENT_ID,
-        clientSecret: process.env.EMAIL_CLIENT_SECRET,
-        refreshToken: process.env.EMAIL_REFRESH_TOKEN,
-        accessToken: token,
-      },
-    });
-
-    await transporter.sendMail({
-      from: `"TradeCatalog" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    });
-  } catch (err) {
-    console.error("Email Error:", err);
-    throw new Error("Email could not be sent");
-  }
-};
-
-module.exports = sendMail;
+try {
+  await transporter.sendMail({
+    from: `"TradeCatalog" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
+} catch (err) {
+  console.error("FULL EMAIL ERROR:", err); // 👈 IMPORTANT
+  throw err; // 👈 don't hide it
+}
