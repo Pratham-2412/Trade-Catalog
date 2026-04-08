0e9eb9637c2c34c920bc6026aa1c88217fe10cc9
Pratham-2412 <pratham17306@gmail.com>
2026-04-08 11:52:55 +0530

    fix backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/User");
 const sendMail = require("../config/nodemailer");
 
 // Generate JWT
